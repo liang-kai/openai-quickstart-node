1ae5f39fbe1557c419b4576d25bea679978462dc
lkaien <625252325@qq.com>
2023-03-31 20:11:07 +0800

    feat: change model

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -33,7 +33,7 @@ export default async function (req, res) {
       max_tokens: 2048
     });
     
-    res.status(200).json({ result: completion.data });
+    res.status(200).json({ result: completion.data.choices[0].text.replaceAll('\n','') });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
