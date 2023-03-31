5c1d64798e7d5644f71efdc18797255d39e89b88
lkaien <625252325@qq.com>
2023-03-31 19:57:22 +0800

    feat: change model

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -27,7 +27,7 @@ export default async function (req, res) {
 
   try {
     const completion = await openai.createCompletion({
-      model: "gpt-3.5-turbo",
+      model: "text-davinci-003",
       prompt: word,
       temperature: 0.6,
     });
